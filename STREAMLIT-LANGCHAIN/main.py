f97a0e80f3f481cf68d23c90e3983a04a65d339c
Mareme Anne <anne.mareme@yahoo.com>
2025-04-28 15:52:44 -0400

    Add main.py for Streamlit app

---
+++ STREAMLIT-LANGCHAIN/main.py
@@ -0,0 +1,29 @@
+from dotenv import load_dotenv
+import os
+import streamlit as st
+from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+
+load_dotenv(override=True)
+
+load_dotenv
+API_KEY = os.getenv('OPENAI_API_KEY')
+
+
+llm = OpenAI(openai_api_key=API_KEY, temperature=0.9)
+
+
+prompt_template = PromptTemplate(
+    template="Give me an example of a meal that could be made using the following ingredients: {ingredients}",
+    input_variables=["ingredients"]
+)
+
+st.title("Meal Planner")
+user_prompt = st.text_input("Enter ingredients (comma-separated):")
+
+if st.button("Generate Meal Idea") and user_prompt: 
+    output = llm(prompt_template.format(ingredients=user_prompt))
+    st.write(output)
+  
+
+
